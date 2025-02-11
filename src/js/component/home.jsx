712f41263123e007269cef7e6f1f342019daf6b2
Anajerar <aurelio.najerar@gmail.com>
2025-02-11 04:30:42 +0000

    Second release, fix based on feedback

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,21 +3,17 @@ import React, { useState } from "react";
 
 const Home = () => {
 
-const [toDo,setToDo] = useState([]);
-const [inputValue, setInputValue] = useState('');
+	const [toDo,setToDo] = useState([]);
+	const [inputValue, setInputValue] = useState('');
 
-function keyHandler(e) {
-	if (e.key==='Enter') {
-		if (e.target.value != ""){
-			setToDo(toDo.concat(inputValue));
-			e.target.value=""
-			setInputValue("")}
-						}
-				}
-
-function taskHoverHandler(e) {
-	e.target.lastChild.hidden=false
-}
+	function keyHandler(e) {
+		if (e.key==='Enter') {
+			if (e.target.value != ""){
+				setToDo(toDo.concat(inputValue));
+				e.target.value=""
+				setInputValue("")}
+							}
+					}
 
 	return (
 		<>
@@ -29,22 +25,25 @@ function taskHoverHandler(e) {
 				onKeyDown={ keyHandler }/>
 			</div>
 				
-					<ul className="list-group list-group-flush tabs-borders">
-						{toDo.map((task,indx)=>{
-							return (
+				<ul className="list-group list-group-flush tabs-borders">
+					{toDo.map((task,indx)=>{
+						return (
 								
-									<li className="list-item d-flex justify-content-between mt-2 pb-2" hidden={true} key={indx}
-										onMouseOver={taskHoverHandler}>
-										<span className="ps-4">{task}</span>
-										<button type="button" className="btn-close pe-5" aria-label="Close"
-										onClick={() => setToDo(toDo.filter((t,thisIndex) => indx != thisIndex)) } hidden={true}></button>
-									</li>								
+								<li className="list-item d-flex justify-content-between mt-2 pb-2" hidden={true} key={indx}
+								onMouseOver={(e) => {if (e.target.lastChild) e.target.lastChild.hidden=false}} 
+								onMouseOut={(e) => {if (e.target.lastChild) e.target.lastChild.hidden=true }}>
+									<span className="ps-4">{task}</span>
+									<button type="button" className="btn-close pe-5" aria-label="Close"
+										onClick={() => setToDo(toDo.filter((t,thisIndex) => indx != thisIndex)) } hidden={true}
+										onMouseOver={(e) => {e.target.hidden=false}} 
+										onMouseOut={(e) => {e.target.hidden=true}}></button>
+								</li>								
 								
-									)
+								)
 						                     })
-						}
+					}
 											
-					</ul>
+				</ul>
 			
 				{toDo.length == 0 ? <p className="tabs-borders">No hay tareas, agrega una </p> : <p className="tabs-boders">{toDo.length} items left</p>}
 			
